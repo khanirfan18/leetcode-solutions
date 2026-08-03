8ce98667986454546831b235769aab078cdda626
irfan khan <ikha4949@gmail.com>
2026-08-03 10:09:47 +0530

    Time: 3 ms (45.73%) | Memory: 23.7 MB (19.26%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,17 +3,12 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        #start, end, step
         i = 0
-        j = len(s) - 1
-        
+        j = len(s)-1
+
         while i < j:
-            s[i],s[j] = s[j],s[i]
+            s[i] , s[j] = s[j] , s[i]
             i+=1
             j-=1
 
         return s
-
-        return s
-
-            
